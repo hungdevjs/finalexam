2792e6b584c312c2c421f39318eed29ce923e322
hungdevjs <hungdev.js@gmail.com>
2020-04-22 20:20:47 +0700

    change timeout to 5s

--- client/src/utils/api/request.js
+++ client/src/utils/api/request.js
@@ -1,24 +1,24 @@
-import axios from "axios";
-import getAccessToken from "../getAccessToken";
-import { BASE_URL } from "../constant";
+import axios from "axios"
+import getAccessToken from "../getAccessToken"
+import { BASE_URL } from "../constant"
 
 const request = axios.create({
     baseURL: BASE_URL,
-    timeout: 3000,
+    timeout: 5000,
     headers: {
         "Content-Type": "application/json",
     },
-});
+})
 
 request.interceptors.request.use(
     (config) => {
-        const token = getAccessToken();
+        const token = getAccessToken()
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
+            config.headers.Authorization = `Bearer ${token}`
         }
-        return config;
+        return config
     },
-    (error) => Promise.reject(error)
-);
+    (error) => Promise.reject(error),
+)
 
-export default request;
+export default request
