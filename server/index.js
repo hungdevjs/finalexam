3f22e06ea144f9b512fccbc2e0a588393e6bc89f
Jack <jack.brightsoft@gmail.com>
2020-04-27 14:17:31 +0700

    add authUser

--- server/index.js
+++ server/index.js
@@ -1,44 +1,47 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const { authUser } = require("./middlewares/auth")
 
-require("dotenv").config();
+require("dotenv").config()
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = process.env.PORT || 8000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cors());
+app.use(cors())
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-});
+    console.log(`server is running on port ${PORT}`)
+})
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-});
+})
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 connection.once("open", () =>
     console.log("MongoDB database connected successfully!")
-);
+)
 
-const logInRoute = require("./routes/logIn.route");
-app.use("/login", logInRoute);
+app.use(authUser)
 
-const userRoute = require("./routes/user.route");
-app.use("/user", userRoute);
+const logInRoute = require("./routes/logIn.route")
+app.use("/login", logInRoute)
 
-const informationRoute = require("./routes/information.route");
-app.use("/information", informationRoute);
+const userRoute = require("./routes/user.route")
+app.use("/user", userRoute)
 
-const gradeRoute = require("./routes/grade.route");
-app.use("/grade", gradeRoute);
+const informationRoute = require("./routes/information.route")
+app.use("/information", informationRoute)
 
-const highlightRoute = require("./routes/highlight.route");
-app.use("/highlight", highlightRoute);
+const gradeRoute = require("./routes/grade.route")
+app.use("/grade", gradeRoute)
+
+const highlightRoute = require("./routes/highlight.route")
+app.use("/highlight", highlightRoute)
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,35 +1,56 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 module.exports.authAdmin = (req, res, next) => {
     const token = req.headers.authorization
         ? req.headers.authorization.split(" ")[1]
-        : "";
+        : ""
 
     try {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
-        const userInfo = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+        const userInfo = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET_KEY)
         if (userInfo.role !== "admin") {
-            throw new Error("Permission is denied");
+            throw new Error("Permission is denied")
         }
-        next();
+        next()
     } catch (err) {
-        res.status(401).send(err.message);
+        res.status(401).send(err.message)
     }
-};
+}
 
 module.exports.authAdminTeacher = (req, res, next) => {
     const token = req.headers.authorization
         ? req.headers.authorization.split(" ")[1]
-        : "";
+        : ""
 
     try {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
-        const userInfo = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+        const userInfo = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET_KEY)
         if (userInfo.role !== "admin" && userInfo !== "teacher") {
-            throw new Error("Permission is denied");
+            throw new Error("Permission is denied")
         }
-        next();
+        next()
     } catch (err) {
-        res.status(401).send(err.message);
+        res.status(401).send(err.message)
     }
-};
+}
+
+module.exports.authUser = (req, res, next) => {
+    const token = req.headers.authorization
+        ? req.headers.authorization.split(" ")[1]
+        : ""
+
+    try {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+        const userInfo = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+        if (
+            userInfo.role !== "admin" &&
+            userInfo !== "teacher" &&
+            userInfo !== "parent"
+        ) {
+            throw new Error("Permission is denied")
+        }
+        next()
+    } catch (err) {
+        res.status(401).send(err.message)
+    }
+}
