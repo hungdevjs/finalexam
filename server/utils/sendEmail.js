4c8b55b5dffd202d78dec162fb6eeb4eab84b7cd
Jack <jack.brightsoft@gmail.com>
2020-05-19 22:48:59 +0700

    fix package name

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,4 +1,4 @@
-const sgMail = require("@sendGrid/mail")
+const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = ({ to, from, subject, text, html }) => {
